65935f23f935d24fd734d4173126a36f804bc43c
禤伟耀 <xuanweiyao@syni.com>
2021-03-28 18:28:36 +0800

    feature: 多表查询模块提交

---
+++ multi-table-example/src/main/java/com/jamie/multi/table/example/mapper/UserMapper.java
@@ -0,0 +1,6 @@
+package com.jamie.multi.table.example.mapper;
+
+import com.jamie.multi.table.example.pojo.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<User> {}
